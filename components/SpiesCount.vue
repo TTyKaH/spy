2b99696a277395f8274caa4208b5652b2c1bd656
vitaly <vitaly.grudachev@servermania.com>
2022-05-20 22:15:39 +0300

    create count components

---
+++ components/SpiesCount.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="set-count flex justify-between items-center gap-2">
+    <div>{{ title }}</div>
+    <div class="flex items-center gap-2">
+      <button class="btn" @click="decrease()">-</button>
+      <div>{{ count }}</div>
+      <button class="btn" @click="increase()">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      count: null,
+    }
+  },
+  mounted() {
+    this.count = Number(localStorage.spiesCount)
+  },
+  methods: {
+    increase() {
+      this.count = this.count + 1
+      localStorage.setItem('spiesCount', this.count)
+    },
+    decrease() {
+      this.count = this.count - 1
+      localStorage.setItem('spiesCount', this.count)
+    },
+  },
+}
+</script>
