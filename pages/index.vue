dbf3bcc3f3733e1b6b8f9f81254bc88f4ca29fb5
vitaly <vitaly.grudachev@servermania.com>
2022-06-06 21:53:28 +0300

    add logo to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="main" class="wrap wrap-py wrap-center text-center">
-    <h1 class="mb-10 text-4xl font-bold">Spy</h1>
+    <div class="grid justify-items-center mb-10">
+      <h1 class="text-4xl font-bold">Spy</h1>
+      <img src="@/static/favicon.png" alt="" width="100px">
+    </div>
     <div class="grid gap-5">
       <NuxtLink to="/settings" class="btn">Начать</NuxtLink>
       <NuxtLink to="#" class="btn">Продолжить</NuxtLink>
