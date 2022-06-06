86bf8e2edfc52175a4ebb4545e2c078aee5f5c19
vitaly <vitaly.grudachev@servermania.com>
2022-06-06 22:29:33 +0300

    polish roles-distribution

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- <NuxtLink to="/">home</NuxtLink> -->
     <Nuxt />
   </div>
 </template>
--- pages/play/roles-distribution.vue
+++ pages/play/roles-distribution.vue
@@ -1,27 +1,40 @@
 <template>
-  <div id="roles-distribution" class="wrap wrap-py">
-    <div v-for="playerNum in playersCount" :key="playerNum" class="text-center">
-      <div v-if="currentPlayer === playerNum">
-        <div v-if="isFirstStage" class="grid justify-items-center gap-5">
-          <div>Игрок {{ playerNum }}</div>
-          <div class="note">
-            Нажмите, чтобы узнать
-            кто ты в этой игре
+  <div id="roles-distribution" class="wrap wrap-py flex flex-col justify-between">
+    <div>
+      <div v-for="playerNum in playersCount" :key="playerNum" class="text-center">
+        <div v-if="currentPlayer === playerNum">
+          <div v-if="isFirstStage" class="flex flex-col justify-between h-auto">
+            <div class="grid justify-items-center gap-5">
+              <div>Игрок {{ playerNum }}</div>
+              <div class="note">
+                Нажмите, чтобы узнать
+                кто ты в этой игре
+              </div>
+            </div>
           </div>
-          <button class="btn" @click="toggle()">Хто я</button>
+          <div v-else class="grid gap-5 justify-items-center">
+            <div class="flex flex-col items-center">
+              <div>Игрок {{ playerNum }}, ты:</div>
+              <div>{{ checkRole(playerNum) }}</div>
+              <img v-if="checkRole(playerNum) === 'Местный'" src="@/assets/images/local.png" width="150px" alt="">
+              <img v-else src="@/assets/images/spy.png" width="150px" alt="">
+            </div>
+            <div v-if="checkRole(playerNum) === 'Местный'" class="note">
+              Локация: {{ selectedLocation }}
+            </div>
+            <div v-else>
+              Все кроме ТЕБЯ знают локацию.
+              Старайся не выдать себя и понять о какой локации все говорят!
+            </div>
+          </div>
         </div>
-        <div v-else class="grid gap-5 justify-items-center">
-          <div>Игрок {{ playerNum }}, ты: {{ checkRole(playerNum) }}</div>
-          <div v-if="checkRole(playerNum) === 'Местный'" class="note">
-            Локация: {{ selectedLocation }}
-          </div>
-          <div v-else>
-            Все кроме ТЕБЯ знают локацию.
-            Старайся не выдать себя и понять о какой локации все говорят
-          </div>
-          <button v-if="currentPlayer !== playersCount" class="btn" @click="nextPlayer()">Понятно</button>
-          <NuxtLink v-else to="/play/time-to-questions" class="btn">Понятно</NuxtLink>
-        </div>
+      </div>
+    </div>
+    <div class="grid">
+      <button v-if="isFirstStage" class="btn" @click="toggle()">Хто я</button>
+      <div v-else class="grid">
+        <button v-if="currentPlayer !== playersCount" class="btn" @click="nextPlayer()">Понятно</button>
+        <NuxtLink v-else to="/play/time-to-questions" class="btn">Понятно</NuxtLink>
       </div>
     </div>
   </div>
