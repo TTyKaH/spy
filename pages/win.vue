199c73588497d7e78aec26a77fcbf40d1ec65768
vitaly <vitaly.grudachev@servermania.com>
2022-06-11 22:43:19 +0300

    upd

--- pages/win.vue
+++ pages/win.vue
@@ -4,15 +4,15 @@
       <div class="text-center">
         <Placeholder :w="200" :h="200" center />
         <h1 class="mt-10 mb-5">{{ whoWin }} победили!</h1>
+        <p>Локация: {{ location }}</p>
         <div class="grid justify-items-center mb-5">
-          <h2>Игроки-шпионы:</h2>
+          <p>Игроки-шпионы:</p>
           <div class="flex gap-2 justify-center flex-wrap">
             <span v-for="(playerNum, idx) in whoSpies" :key="idx" class="btn">
               {{ playerNum }}
             </span>
           </div>
         </div>
-        <p>Локация: {{ location }}</p>
       </div>
       <NuxtLink class="btn" to="/">На главную</NuxtLink>
     </div>
