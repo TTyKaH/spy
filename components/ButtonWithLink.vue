4b2073eeb66b7461349e2cc5cc5d407b587187ba
vitaly <vitaly.grudachev@servermania.com>
2022-07-11 22:32:01 +0300

    add ButtonWithLink component

---
+++ components/ButtonWithLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="handler">
+    <NuxtLink :to="to" class="btn block w-full">
+      <slot></slot>
+    </NuxtLink>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: '/'
+    },
+    handler: {
+      type: Function,
+      default: () => {}
+    }
+  }
+}
+</script>
