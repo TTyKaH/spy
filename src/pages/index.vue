83bd192b6784814baf787202976822208204a80b
vitaly <vitaly.grudachev@servermania.com>
2023-02-10 22:56:17 +0300

    upd styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,12 +3,16 @@
     <DarkModeToggle class="absolute top-0 right-0 z-10"></DarkModeToggle>
     <div class="grid justify-items-center mb-10">
       <h1 class="text-4xl font-bold">Spy</h1>
-      <img src="@/static/favicon.png" alt="" width="100px">
+      <img src="@/static/favicon.png" alt="" width="100px" />
     </div>
     <div class="grid gap-5">
       <ButtonWithLink to="/settings">Начать</ButtonWithLink>
-      <ButtonWithLink v-if="!canResume" :disabled="true">Продолжить</ButtonWithLink>
-      <ButtonWithLink v-else to="/play/time-to-questions">Продолжить</ButtonWithLink>
+      <ButtonWithLink v-if="!canResume" :disabled="true">
+        Продолжить
+      </ButtonWithLink>
+      <ButtonWithLink v-else to="/play/time-to-questions">
+        Продолжить
+      </ButtonWithLink>
       <ButtonWithLink to="/rules">Правила</ButtonWithLink>
     </div>
   </div>
