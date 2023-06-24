5d8de110dcd240a7a2b94e534e218e01b028bc45
vitaly <vitaly.grudachev@servermania.com>
2023-06-24 14:00:47 +0300

    fix style lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,9 +3,16 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recommended-vue',
-    'stylelint-config-prettier',
+    'stylelint-config-prettier'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'deep']
+      }
+    ]
+  }
 }
